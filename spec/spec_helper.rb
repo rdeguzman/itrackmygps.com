ed0127e1cd36164270778e1e20723a021a0fc964
rupert <rupert@2rmobile.com>
2014-01-05 13:56:12 +1100

    Disable randomness in tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -39,7 +39,7 @@ RSpec.configure do |config|
   # order dependency and want to debug it, you can fix the order by providing
   # the seed, which is printed after each run.
   #     --seed 1234
-  config.order = "random"
+  #config.order = "random"
 
   config.color_enabled = true
   config.filter_run :focus => true
