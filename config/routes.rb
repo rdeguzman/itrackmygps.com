f452aed8e225a03069c285fc1a22c11cc0fe1e05
rupert <rupert@2rmobile.com>
2014-01-05 13:52:25 +1100

    Create only devise registration routes for API

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Trackble::Application.routes.draw do
   root :to => "home#index"
-
-  namespace :api do
-    devise_for :users
-  end
-
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 
+  namespace :api do
+    devise_for :users, :only => :registrations
+  end
 end
