86b70fb2308adae38364efcf772ef844234b157b
rupert <rupert@2rmobile.com>
2014-01-05 13:55:19 +1100

    Include capybara in spec_helper. Set rspec tests to fail fast

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,6 +4,7 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'email_spec'
 require 'rspec/autorun'
+require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
@@ -19,6 +20,7 @@ RSpec.configure do |config|
   # config.mock_with :mocha
   # config.mock_with :flexmock
   # config.mock_with :rr
+  config.mock_with :rspec
 
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -38,7 +40,13 @@ RSpec.configure do |config|
   # the seed, which is printed after each run.
   #     --seed 1234
   config.order = "random"
-  
+
+  config.color_enabled = true
+  config.filter_run :focus => true
+  config.filter_run_excluding :wip => true
+  config.run_all_when_everything_filtered = true
+  config.fail_fast = true
+
   config.before(:suite) do
     DatabaseCleaner.strategy = :truncation
   end
