186535db0d14513213e14da64b312334b1be965b
rupert <rupert@2rmobile.com>
2014-01-18 17:52:03 +1100

    Lazily instantiate a variable using “let”. See betterspecs.org http://betterspecs.org/#let

--- spec/support/shared_context_for_login.rb
+++ spec/support/shared_context_for_login.rb
@@ -1,10 +1,10 @@
 shared_context 'login' do
-  def login_user
-    @user = FactoryGirl.create(:user)
+  let(:user) { FactoryGirl.create :user }
 
+  def login_user
     visit new_user_session_path
-    fill_in 'Username', :with => @user.username
-    fill_in 'Password', :with => @user.password
+    fill_in 'Username', :with => user.username
+    fill_in 'Password', :with => user.password
 
     click_button 'Submit'
     #save_and_open_page
