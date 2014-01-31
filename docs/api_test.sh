ed810c9e2b971f5cb0c772e380d1fcac6f1028b6
rupert <rupert@2rmobile.com>
2014-01-31 13:49:14 +1100

    Updated login in api_test.sh

--- docs/api_test.sh
+++ docs/api_test.sh
@@ -1,2 +1,2 @@
-curl -i -H "Content-Type applicationjson" -X POST --data 'user[username]=rupert&user[email]=rupert@2rmobile.com&user[password]=junjunmalupet&user[password_confirmation]=junjunmalupet' http://127.0.0.1:3000/api/users.json
-curl -i -H "Content-Type applicationjson" -X POST --data 'username=rupert&password=junjunmalupet' http://127.0.0.1:3000/api/users/sign_in.json
+#curl -i -H "Content-Type applicationjson" -X POST --data 'user[username]=rupert&user[email]=rupert@2rmobile.com&user[password]=junjunmalupet&user[password_confirmation]=junjunmalupet' http://127.0.0.1:3000/api/login
+curl -i -H "Content-Type applicationjson" -X POST --data 'username=rupert&password=junjunmalupet&uuid=a9cafa29-8378-351a-9843-b8201241e1d0' http://trackble.geocoding.io/api/login
