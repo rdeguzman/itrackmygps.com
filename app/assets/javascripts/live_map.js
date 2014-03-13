ffea425da365b57af2503477189adac828bc6c0a
rupert <rupert@2rmobile.com>
2014-03-13 21:43:21 +1100

    Refactor javascript for mapper#current to a separate external live_map.js

---
+++ app/assets/javascripts/live_map.js
@@ -0,0 +1,127 @@
+var map, devices_bounds;
+var devices = [];
+
+function initMap(){
+  var myLatlng = new google.maps.LatLng(0,0);
+
+  var mapOptions = {
+    center: myLatlng,
+    zoom: 10
+  };
+
+  map = new google.maps.Map(document.getElementById('mapdiv'), mapOptions);
+  devices_bounds = new google.maps.LatLngBounds();
+}
+
+function getDevice(device_id, markers){
+  var device = null;
+
+  for(var i = 0; i < markers.length; i++){
+    if(markers[i].device_id === device_id){
+      device = markers[i];
+      break;
+    }
+  }
+
+  return device;
+}
+
+function processGPS(gps){
+  var device = getDevice(gps.device_id, devices);
+  if(device == null){
+    device = createDevice(gps);
+    devices.push(device);
+  }
+  else{
+    moveDevice(device, gps);
+  }
+}
+
+function createDevice(gps){
+  var pos = getLatLngFromString(gps);
+  var marker = new google.maps.Marker({
+    position: pos,
+    map: map
+  });
+
+  marker.set("device_id", gps.device_id);
+  adjustMapBounds(pos);
+
+  return marker;
+}
+
+function moveDevice(device, gps){
+  var pos = getLatLngFromString(gps);
+  device.setPosition(pos);
+  adjustMapBounds(pos);
+}
+
+function adjustMapBounds(pos){
+  // need to wait for the tiles to load before we can get the map bounds
+  var currentBounds = map.getBounds();
+
+  if(typeof(currentBounds) != 'undefined' &&  currentBounds.contains(pos) == false){
+    devices_bounds.extend(pos);
+    map.fitBounds(devices_bounds);
+  }
+}
+
+function fitAllDevicesOnMap(){
+  for(var i = 0; i < devices.length; i++){
+    var pos = devices[i].getPosition();
+    devices_bounds.extend(pos);
+    map.fitBounds(devices_bounds);
+  }
+}
+
+function getLatLngFromString(obj){
+  var lat = parseFloat(obj.gps_latitude), lon = parseFloat(obj.gps_longitude);
+  return new google.maps.LatLng(lat, lon);
+}
+
+function formatGPSHTMLOutput(gps){
+  var unix_timestamp = gps.gps_timestamp;
+  var date = new Date(unix_timestamp*1);
+  var formattedTime = date.toLocaleString();
+
+  var s =  formattedTime + ' ';
+  s += '(' + gps.gps_latitude + ',' + gps.gps_longitude + ')';
+  return s;
+}
+
+function fetchLastKnownDeviceLocation(data){
+  for(var i=0;i < data.length;i++){
+    var gps = data[i];
+    if(gps.gps_latitude != 0 && gps.gps_longitude != 0){
+      device = createDevice(data[i]);
+      devices.push(device);
+
+      $('#messages').html(formatGPSHTMLOutput(gps));
+    }
+  }
+
+  fitAllDevicesOnMap();
+}
+
+function initSocket(socket_url, user_id, data){
+  var socket = io.connect(socket_url);
+
+  socket.on('connect', function () {
+      // Identify this socket with the user_id
+    socket.emit('setUserId', user_id);
+
+    for(var i=0;i < data.length;i++){
+      socket.emit('addDevice', data[i].uuid);
+    }
+
+  });
+
+  socket.on('message', function(d){
+    var parsedObj = JSON.parse(d);
+    if(parsedObj.type === 'gps'){
+      var gps = parsedObj.data;
+      $('#messages').html(formatGPSHTMLOutput(gps));
+      processGPS(gps);
+    }
+  });
+}
