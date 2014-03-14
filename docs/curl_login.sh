461e8c0958556c09f555b3f4e99a0424fa967a01
rupert <rupert@2rmobile.com>
2014-03-15 07:26:31 +1100

    Split CURL API test script for register and login

---
+++ docs/curl_login.sh
@@ -0,0 +1,1 @@
+curl -i -H "Content-Type applicationjson" -X POST --data 'username=rupert&password=junjunmalupet&uuid=a9cafa29-8378-351a-9843-b8201241e1d0' http://itrackmygps.com/api/login
---
+++ docs/curl_register.sh
@@ -0,0 +1,1 @@
+curl -i -H "Content-Type applicationjson" -X POST --data 'user[username]=rupert&user[email]=rupert@2rmobile.com&user[password]=junjunmalupet&user[password_confirmation]=junjunmalupet' http://127.0.0.1:3000/api/register
