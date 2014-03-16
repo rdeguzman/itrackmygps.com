60b01e71682152c2433505f5e204777667b1bb9c
rupert <rupert@2rmobile.com>
2014-03-16 22:29:38 +1100

    get /devices should only be accessed when logged in

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -1,32 +1,7 @@
 class DevicesController < ApplicationController
   def index
-    if user_signed_in?
-      @devices = get_devices(current_user.id)
-    else
-      jsonData = {}
-
-      users = User.where(:id => params[:user_id], :access_token => params[:access_token])
-
-      if users.empty?
-        jsonData[:valid] = false
-        jsonData[:error] = 'Invalid access token'
-      else
-        user = users.first
-        devices = get_devices(user.id)
-
-        jsonData[:valid] = true
-        jsonData[:devices] = devices
-      end
-    end
-
-    respond_to do |format|
-        format.html # index.html.erb
-        format.json { render :json => jsonData }
-    end
+    authenticate_user!
+    @devices = Device.where(:user_id => current_user.id)
   end
 
-  private
-    def get_devices(user_id)
-      Device.where(:user_id => user_id)
-    end
 end
