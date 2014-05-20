48a1ad1e372a2b37baff87bfc476daba2efc530b
rupert <rupert@2rmobile.com>
2014-05-21 06:50:01 +1000

    Add pg_dump and restore instructions in tools/pg_dump.sh

--- tools/pg_dump.sh
+++ tools/pg_dump.sh
@@ -1,1 +1,9 @@
-pg_dump -h robin -U rupert -W -t locations -a -f locations.backup gpslogger_production 
+# Dumps location table only
+# pg_dump -h robin -U rupert -W -t locations -a -f locations.backup itrackmygps_development
+
+# Dumps whole database
+pg_dump -h robin -U rupert -W -f itrackmygps.backup itrackmygps_production
+
+# Loads the database
+echo "To load the data:"
+echo "psql -h 127.0.0.1 -U rupert -d itrackmygps_development -f itrackmygps.backup"
