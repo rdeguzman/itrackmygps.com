ac158e0e5cbd95c84f248711f3a8e23aad3ffdf6
rupert <rupert@2rmobile.com>
2014-05-21 07:27:07 +1000

    See #2, add select_tag devices for location history

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -2,6 +2,6 @@ class ReportsController < ApplicationController
   before_filter :authenticate_user!
 
   def gps_activity
-
+    @devices = Device.where(:user_id => current_user.id)
   end
 end
--- spec/features/location_history_spec.rb
+++ spec/features/location_history_spec.rb
@@ -7,6 +7,12 @@ describe 'Location History Page' do
     login_user
 
     click_link "Location History"
+
+    page.should have_select "Devices"
+    #page.should have_field "From"
+    #page.should have_field "To"
+    page.should have_button "Submit"
+
     #save_and_open_page
 
     logout
