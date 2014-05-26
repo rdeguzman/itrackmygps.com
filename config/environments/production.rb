f01bcab30c4766b671c5e920e5b84a9c948d472e
rupert <rupert@2rmobile.com>
2014-05-27 06:44:57 +1000

    Add default_url_options in development.rb and production.rb to fix "Missing host to link to! Please provide the :host parameter, set default_url_options[:host], or set :only_path to true"

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,8 @@ Itrackmygps::Application.configure do
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
+  config.action_mailer.default_url_options = { :host => "itrackmygps.com" }
+
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
