d6253baeb5cda5c85ef02a153fe4b0763eeecaf9
rupert <rupert@2rmobile.com>
2014-05-27 14:30:28 +1000

    Update mapper spec  because of changes in devise.en.yml

--- spec/features/mapper_spec.rb
+++ spec/features/mapper_spec.rb
@@ -6,7 +6,7 @@ describe 'Map' do
   context 'not logged in' do
     it 'should authenticate on /current' do
       visit current_map_path
-      page.should have_content "You need to sign in or sign up before continuing."
+      page.should have_content "You need to log in or register before continuing."
       page.should have_link "Login"
       page.should have_link "Sign Up"
     end
