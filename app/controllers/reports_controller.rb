bb0782bccdead8571295e80dc1fa3913139a7c17
rupert <rupert@2rmobile.com>
2014-06-07 19:39:20 +1000

    Set default APP_CONFIG[:per_page] to 100

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -19,14 +19,14 @@ class ReportsController < ApplicationController
                                     @first_timestamp).
                                     order('created_at asc').
                                     paginate(:page => params[:page],
-                                             :per_page => params[:per_page] || 10)
+                                             :per_page => params[:per_page] || APP_CONFIG[:per_page])
       else
         @locations = Location.where('uuid = ? AND gps_timestamp >= ? AND gps_timestamp <= ?',
                                     device.uuid,
                                     params[:date_from],
                                     params[:date_to]).order('created_at asc').
                                     paginate(:page => params[:page],
-                                             :per_page => params[:per_page] || 10)
+                                             :per_page => params[:per_page] || APP_CONFIG[:per_page])
       end
     else
       @locations = []
