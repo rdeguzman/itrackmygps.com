a6db11ce11bebd70adc3e2fc5b69ea424a07fe48
rupert <rupert@2rmobile.com>
2016-06-04 12:04:14 +1000

    Add devices#destroy

--- app/controllers/devices_controller.rb
+++ app/controllers/devices_controller.rb
@@ -4,4 +4,10 @@ class DevicesController < ApplicationController
     @devices = Device.where(:user_id => current_user.id)
   end
 
+  def destroy
+    device = Device.find(params[:id])
+    device.destroy
+    redirect_to devices_path
+  end
+
 end
